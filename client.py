71965807bb55273d86d85ed3bb645e99057e3512
wendy carolina iza gualiche <wendy@zeta40.aulas.gsyc.es>
2015-10-26 16:58:34 +0100

    client.py 6

--- client.py
+++ client.py
@@ -16,7 +16,10 @@ if PORT < 1024:
     sys.exit('ERROR: PUERTO NO VÁLIDO')
 
 # Contenido que vamos a enviar
-LINE = ' '.join(sys.argv[3:])
+Linea = sys.argv[3:]
+LINE = ' '.join(Linea)
+
+
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -26,6 +29,10 @@ my_socket.connect((SERVER, PORT))
 print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
+if Linea[0] == 'REGISTER':
+    my_socket.send(b'REGISTER sip: ' + bytes(Linea[0],'utf-8') + b'SIP/2.0\r\n\r\n')
+   
+
 
 print('Recibido -- ', data.decode('utf-8'))
 print("Terminando socket...")
