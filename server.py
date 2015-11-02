47b5ffca9e29149ec4325ae46b95cbeaa3a26ec6
Wendy Carolina Iza <carolina.iza.g@gmail.com>
2015-11-02 19:49:03 +0100

    comprueba fichero json

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ import socketserver
 import sys
 import time
 import json
+import os.path
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
@@ -16,11 +17,18 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     dicc = {}
     def register2json(self):
         fich = json.dumps(self.dicc)
-        with open('register.json', 'w') as fich:
+        with open('registered.json', 'w') as fich:
             json.dump(self.dicc, fich, sort_keys=True, indent=4)
+
+    def json2registered(self):
+        if os.path.exists('registered.json'):
+            self.dicc = json.loads(open('registered.json').read())
+        else:
+            self.dicc = {}
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
+        self.json2registered()
         print('IP: ' + self.client_address[0])
         print('Port: ' + str(self.client_address[1]))
         while 1:
@@ -30,6 +38,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             #print(linea)
             if len(linea.split()) >= 2:
                 if linea.split()[0] == 'REGISTER':
+                    #Creo y guardo datos usuario
                     Usuario = linea.split()[2]
                     IP = self.client_address[0]
                     Expires = float(linea.split(':')[-1])
@@ -37,6 +46,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                     Time_expire = Time + Expires 
                     Time_user = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(Time_expire))
                     Datos_User = [IP, Time_user]
+                    #Compruebo si el usuario se ha expiradoo se da de baja
                     for Usuario in self.dicc:
                         Time = time.time()
                         if Time >= Time_expire or Expires == 0:
@@ -47,12 +57,12 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                     self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
                 elif line.split()[0] != 'REGISTER':
                     print("El cliente nos manda " + linea)
-
             
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
 
+    
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     PORT = int(sys.argv[1])
