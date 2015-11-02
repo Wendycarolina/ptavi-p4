fa2f8bb4504fe063e5f99bf139892716cd76ca26
Wendy Carolina Iza <carolina.iza.g@gmail.com>
2015-11-02 03:33:11 +0100

    json

--- server.py
+++ server.py
@@ -6,12 +6,19 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+import time
+import json
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
     dicc = {}
+    def register2json(self):
+        fich = json.dumps(self.dicc)
+        with open('register.json', 'w') as fich:
+            json.dump(self.dicc, fich, sort_keys=True, indent=4)
+
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write(b"Hemos recibido tu peticion ")
@@ -20,14 +27,17 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
+            
             if len(line.split()) >= 2:
               
                 if line.split()[0] == 'REGISTER':
-                    dicc[line[2]] = self.client_address[0]
+                    self.dicc[line[2]] = self.client_address[0]
+                    self.register2json()
                     Time = line.split()[5]
                     if  Time == '0':
-                        del dicc[line[2]]
-                        self.wfile.write('SIP/2.0 200 OK\r\n\r\n')
+                        del self.dicc[line[2]]
+                        self.register2json()
+                        self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
                 elif line.split()[0] != 'REGISTER':
                     print("El cliente nos manda " + line.decode('utf-8'))
 
